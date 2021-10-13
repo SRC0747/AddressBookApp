c5b6be444a09e9dce15cc04d4c7d81f810c970f3
SRC0747 <RjSrc@0747>
2021-10-13 08:24:56 +0530

    Welcome to AddressBook App Controller.

---
+++ src/test/java/com/bridgeLabz/AddressBook/App/AddressBookAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgeLabz.AddressBook.App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AddressBookAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
